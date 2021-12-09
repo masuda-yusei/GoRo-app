60c3f0d1b9d779b8c1c722a906d49517485f7071
yusei masuda <masuda.no@icloud.com>
2021-12-09 22:35:11 +0900

    プロフィール編集機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :blogs
-  devise_for :users
+  devise_for :users,
+    controllers: { registrations: 'registrations' }
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
