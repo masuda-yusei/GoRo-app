cff5d715db0073a3fe5377c6477173c6caa21085
yusei masuda <masuda.no@icloud.com>
2021-12-10 12:03:45 +0900

    ブログ機能追加 #4

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,10 @@ ActiveRecord::Schema.define(version: 2021_12_09_050304) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +43,5 @@ ActiveRecord::Schema.define(version: 2021_12_09_050304) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "blogs", "users"
 end
