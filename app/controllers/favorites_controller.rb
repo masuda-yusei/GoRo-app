efd90a2889ccb181039c673e6093c4f19361e494
yusei masuda <masuda.no@icloud.com>
2021-12-10 14:11:12 +0900

    微修正済み

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,11 @@
 class FavoritesController < ApplicationController
   def create
-    favorite = current_user.favorites.create(trip_id: params[:trip_id])
-    redirect_to trips_path, notice: "#{favorite.trip.user.name}の投稿をいいねしました"
+    favorite = current_user.favorites.create(user_id: params[:user_id])
+    redirect_to users_path, notice: "#{favorite.user.name}の投稿をいいねしました"
   end
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to trips_path, notice: "#{favorite.trip.user.name}の投稿のいいねを解除しました"
+    redirect_to users_path, notice: "#{favorite.user.name}の投稿のいいねを解除しました"
   end
 end
