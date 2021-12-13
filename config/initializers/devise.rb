5588381ab212cf22d7f47c6e23919310378d3c95
yusei masuda <masuda.no@icloud.com>
2021-12-13 17:43:08 +0900

    メーラー機能追加 #4

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = 'acd8f770013e9999cbc0daf7edd9be9179e394e5b1f1e3958a31e018888f115d8f98fe937d7480b2dc62f45da772655874d46b35f25c56dde6bef24976468471'
+  config.secret_key = 'acd8f770013e9999cbc0daf7edd9be9179e394e5b1f1e3958a31e018888f115d8f98fe937d7480b2dc62f45da772655874d46b35f25c56dde6bef24976468471'
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'masuda.no@icloud.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
--- db/schema.rb
+++ db/schema.rb
@@ -85,9 +85,14 @@ ActiveRecord::Schema.define(version: 2021_12_12_020228) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
