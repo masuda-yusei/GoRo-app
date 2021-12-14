0ff1d6d2423625c5a4b830c491cfd00dab9412a4
yusei masuda <masuda.no@icloud.com>
2021-12-14 13:10:30 +0900

    header実装 #10

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -49,7 +49,7 @@ class ProfilesController < ApplicationController
 
   private
     def set_profile
-      @profile = Profile.find(params[:id])
+      @profile = Profile.find_by(user_id:params[:id])
     end
 
     def profile_params
