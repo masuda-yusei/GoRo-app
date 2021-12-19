2f6b73287f47e5c34a9de390dd717e7cc62387d6
yusei masuda <masuda.no@icloud.com>
2021-12-20 01:00:11 +0900

    seedファイル修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 admin = User.create!(
   name:"GoRo公式アカウント",
-  email:"test15@gmail.com",
+  email:"admin@gmail.com",
   password:"111111",
   admin: true,
   confirmed_at: Time.now
@@ -8,135 +8,111 @@ admin = User.create!(
 
 user1 = User.create!(
   name: "Yusei",
-  email: "test16@gmail.com",
+  email: "7575you@gmail.com",
   password: "111111",
   admin: true,
   confirmed_at: Time.now
 )
 user2 = User.create!(
   name: "Yoshiki",
-  email: "test11@gmail.com",
+  email: "test1@gmail.com",
   password: "111111",
   confirmed_at: Time.now
 )
 user3 = User.create!(
   name: "Yamada",
-  email: "test12@gmail.com",
+  email: "test2@gmail.com",
   password: "111111",
   confirmed_at: Time.now
 )
 user4 = User.create!(
   name: "Toshi",
-  email: "test13@gmail.com",
+  email: "test3@gmail.com",
   password: "111111",
   confirmed_at: Time.now
 )
 user5 = User.create!(
   name: "Kei",
-  email: "test14@gmail.com",
+  email: "test4@gmail.com",
   password: "111111",
   confirmed_at: Time.now
 )
 
 profile1 = user1.build_profile(
   # id: user1.id,
-  icon: "profile.jpg",
+  icon: "profile1.jpg",
   gender: "男性",
   birthday: "1994-09-02",
   residence: 1,
+  golf_history: 6,
+  average_score: 5,
+  annual_round_time: 10,
+  playable_date: 3,
+  pick_up: 2,
   introduction: "学生時代からゴルフを始めました！　よろしくお願いします！"
 )
 profile1.save
 
 profile2 = user2.build_profile(
   # id: user2.id,
-  icon: "profile.jpg",
-  gender: "男性",
+  icon: "profile2.jpg",
+  gender: "女性",
   birthday: "1995-08-02",
   residence: 2,
-  introduction: "ベストスコア80です！　よろしくお願いします！"
+  golf_history: 4,
+  average_score: 8,
+  annual_round_time: 8,
+  playable_date: 2,
+  pick_up: 1,
+  introduction: "ベストスコア90です！　よろしくお願いします！"
 )
 profile2.save
 
 profile3 = user3.build_profile(
   # id: user3.id,
-  icon: "profile.jpg",
+  icon: "profile3.jpg",
   gender: "男性",
   birthday: "1996-07-11",
   residence: 3,
+  golf_history: 2,
+  average_score: 7,
+  annual_round_time: 7,
+  playable_date: 3,
+  pick_up: 2,
   introduction: "一緒にラウンドしましょう！"
 )
 profile3.save
 
 profile4 = user4.build_profile(
   # id: user4.id,
-  icon: "profile.jpg",
+  icon: "profile4.jpg",
   gender: "男性",
   birthday: "1990-07-11",
   residence: 4,
-  introduction: "最近趣味でゴルフ始めました！"
+  golf_history: 1,
+  average_score: 11,
+  annual_round_time: 3,
+  playable_date: 3,
+  pick_up: 2,
+  introduction: "最近趣味でゴルフ始めました！ 一緒にラウンド楽しみましょう！"
 )
 profile4.save
 
 profile5 = user5.build_profile(
   # id: user5.id,
-  icon: "profile.jpg",
+  icon: "profile5.jpg",
   gender: "男性",
   birthday: "1988-07-11",
   residence: 5,
+  golf_history: 1,
+  average_score: 12,
+  annual_round_time: 4,
+  playable_date: 1,
+  pick_up: 1,
   introduction: "初心者ですが、よろしくお願いします！"
 )
 profile5.save
 
-# golf_information1 = user1.build_golf_information(
-#   id: user1.id,
-#   Golf_History: "5年以上",
-#   Average_Score: "85",
-#   Annual_Round_Times: "10回",
-#   Playable_Date: "2021-12-24",
-#   Pick_Up: "自分でゴルフ場に行けます"
-# )
-# golf_information1.save
-
-# golf_information2 = user2.build_golf_information(
-#   id: user2.id,
-#   Golf_History: "2年",
-#   Average_Score: "90",
-#   Annual_Round_Times: "6回",
-#   Playable_Date: "2021-12-25",
-#   Pick_Up: "送迎お願いします"
-# )
-# golf_information2.save
-
-# golf_information3 = user3.build_golf_information(
-#   id: user3.id,
-#   Golf_History: "3年",
-#   Average_Score: "95",
-#   Annual_Round_Times: "4回",
-#   Playable_Date: "2021-12-26",
-#   Pick_Up: "送迎お願いします"
-# )
-# golf_information3.save
-
-# golf_information4 = user4.build_golf_information(
-#   id: user4.id,
-#   Golf_History: "10年",
-#   Average_Score: "75",
-#   Annual_Round_Times: "20回",
-#   Playable_Date: "2021-12-27",
-#   Pick_Up: "自分でゴルフ場に行けます"
-# )
-# golf_information4.save
-
-# golf_information5 = user5.build_golf_information(
-#   id: user5.id,
-#   Golf_History: "1年未満",
-#   Average_Score: "110",
-#   Annual_Round_Times: "2回",
-#   Playable_Date: "2021-12-28",
-#   Pick_Up: "送迎お願いします"
-# )
-# golf_information5.save
 
 Favorite.create!(
   user_id: user1.id,
