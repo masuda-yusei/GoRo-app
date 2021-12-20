ad901a4f651ca8ffb15e9d83d849a85387709e13
yusei masuda <masuda.no@icloud.com>
2021-12-20 18:16:11 +0900

    メール送信機能が使用できるように修正

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ Rails.application.routes.draw do
   # resources :users
 
 
-  if Rails.env.development?
+  # if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  end
+  # end
 end
