d4d0a5d02ea75f84e539d03a518c554a5dcae213
yusei masuda <masuda.no@icloud.com>
2021-12-20 14:11:33 +0900

    herokuでSendGridが使用できるよう修正

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ Rails.application.routes.draw do
   # resources :users
 
 
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  end
+  # if Rails.env.development?
+  #   mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  # end
 end
