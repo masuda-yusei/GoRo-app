3c0d3ba7809b35665706db7af05b356f99670e25
yusei masuda <masuda.no@icloud.com>
2022-03-08 14:53:06 +0900

    エンドポイント修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,7 +14,8 @@ CarrierWave.configure do |config|
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # アクセスキー
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # シークレットアクセスキー
       region: 'ap-northeast-1', # リージョン
-      endpoint: 'https://aws-s3-bucket-you.s3-ap-northeast-1.amazonaws.com/',
+      # endpoint: 'http://aws-s3-bucket-you.s3-ap-northeast-1.amazonaws.com/',
+      endpoint: 'http://aws-s3-bucket-you.s3-website-ap-northeast-1.amazonaws.com',
       path_style: true
     }
   else # 本番環境以外の場合はアプリケーション内にアップロード
